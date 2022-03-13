2c10dba9274f251beed590098acc956ed26fc6bc
Carlos Soria <soriac367@gmail.com>
2022-03-12 22:02:06 -0500

    More prototypes added

--- main.h
+++ main.h
@@ -1,8 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int _printf(const char *format, ...);
-int str_len(const char *str);
+int str_len(char *str);
+int f_select(const char *format, char *fs, char *s, int sz, va_list args, int z);
+void print_char(const char *format, char *f_s, int sz, va_list args, int num_opt, int z);
+char *fs_init(char *s, int n);
 
 #endif /*MAIN_H*/
 
