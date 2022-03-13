04f56c0d7b2237494375ad922b49fa24dd32029e
Carlos Soria <soriac367@gmail.com>
2022-03-12 22:05:48 -0500

    Function that selects a format case

---
+++ f_select.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+int f_select(const char *format, char *fs, char *s, int sz, va_list args, int z)
+{
+	int num_opt;
+	
+	if (str_len(s) == 0)
+		return (0);
+	for (num_opt = 0; s[num_opt] < 'a'; num_opt++)
+		;
+	switch(s[num_opt])
+	{
+		case 'c':
+			print_char(format, fs, sz, args, num_opt, z);
+			break;
+	}
+	return (0);
+}
