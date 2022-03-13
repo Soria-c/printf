b33e84b960c8d3971129abf706a08eedf6ce6a9a
Carlos Soria <soriac367@gmail.com>
2022-03-12 22:36:01 -0500

    Added prototype to print formated digits

--- main.h
+++ main.h
@@ -10,6 +10,7 @@ int _printf(const char *format, ...);
 int str_len(char *str);
 int f_select(const char *format, char *fs, char *s, int sz, va_list args, int z);
 void print_char(const char *format, char *f_s, int sz, va_list args, int num_opt, int z);
+void print_digit(const char *format, char *f_s, int sz, va_list args, int num_opt, int z);
 char *fs_init(char *s, int n);
 
 #endif /*MAIN_H*/
