87228d53de9ac4de8496881c6a8878fabd5a6a59
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-14 13:49:25 -0700

    Now handles %d

--- print_digit.c
+++ print_digit.c
@@ -1,21 +1,40 @@
 #include "main.h"
 
-void print_digit(const char *format, char *f_s, int sz, va_list args, int num_opt, int z)
-{
-	int copy, i, j;
-	char *f_check;
+/**
+ * print_d - copies args in f
+ * @format: input string
+ * @f: buffer
+ * @sz: number of bytes before %
+ * @args: arguments list
+ * @op: operation number
+ * @z: offset
+ *
+ * Return: 0 if f_sel is SUCCESS and 1 otherwise.
+ */
 
+int print_d(const char *format, char *f, int sz, va_list args, int op, int z)
+{
+	int copy, i, j, k, r;
+	char *f_check, *string;
+
+	r = 0;
 	for (copy = 0; copy < sz; copy++)
-		f_s[copy] = format[z + copy];
-	f_s[copy] = va_arg(args, int) + '0';
-	j = copy + 1;
-	for (; copy + 1 < str_len(f_s); copy++)
-		f_s[copy + 1] = format[copy + z + num_opt + 1];
-	f_s[copy + 1] = '\0';
-	f_check = f_s;
-	f_s = f_s + j;
+		f[copy] = format[z + copy];
+	string = to_string(va_arg(args, int));
+	for (k = 0; string[k] != '\0'; k++)
+		f[copy + k] = string[k];
+	j = copy + k;
+	free(string);
+	for (; format[copy + z + op + 1] != '\0'; copy++)
+		f[copy + k] = format[copy + z + op + 1];
+	f[copy + k] = '\0';
+	f = f + j;
+	f_check = f;
 	for (i = 0; *f_check != '%' && *f_check != 0; i++, f_check++)
-		;
-	z = z + sz + num_opt + 1;
-	f_select(format, f_s, f_check, i - j, args, z);
+		continue;
+	z = z + sz + op + 1;
+	r = f_sel(format, f, f_check, i, args, z);
+	if (r == 1)
+		return (r);
+	return (r);
 }
