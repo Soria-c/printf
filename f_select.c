e909d10ec4951b00a2c3202eb912a21b2553a1a9
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-14 13:45:15 -0700

    Added description and betty style

--- f_select.c
+++ f_select.c
@@ -1,21 +1,51 @@
 #include "main.h"
 
-int f_select(const char *format, char *fs, char *s, int sz, va_list args, int z)
+/**
+ * f_sel - selects format case
+ * @format: input string
+ * @fs: buffer
+ * @s: address of first incidence of % in fs
+ * @sz: number of bytes before %
+ * @args: arguments list
+ * @z: offset
+ *
+ * Return: 0 if f_sel is SUCCESS and 1 otherwise.
+ */
+
+int f_sel(const char *format, char *fs, char *s, int sz, va_list args, int z)
 {
-	int num_opt;
-	
-	if (str_len(s) == 0)
-		return (0);
-	for (num_opt = 0; s[num_opt] < 'a'; num_opt++)
-		;
-	switch(s[num_opt])
+	int op, r;
+
+	r = 0;
+	for (op = 0; s[op] < 'a'; op++)
+		continue;
+	if (s[1] == '%')
+		op = 1;
+	switch (s[op])
 	{
 		case 'c':
-			print_char(format, fs, sz, args, num_opt, z);
+			r = print_c(format, fs, sz, args, op, z);
 			break;
 		case 'd':
-			print_digit(format, fs, sz, args, num_opt, z);
+			r = print_d(format, fs, sz, args, op, z);
 			break;
+		case 'i':
+			r = print_d(format, fs, sz, args, op, z);
+			break;
+		case '%':
+			r = print_p(format, fs, sz, args, op, z);
+			break;
+		case 's':
+			r = print_s(format, fs, sz, args, op, z);
+			break;
+		default:
+			if (*s == '\0')
+				return (0);
+			return (1);
+	}
+	if (r == 1)
+	{
+		return (1);
 	}
 	return (0);
 }
