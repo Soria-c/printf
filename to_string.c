220c72b46992b9e4b63bfacafec628d14e00474d
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-14 13:47:19 -0700

    Now handles %s

---
+++ to_string.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * to_string - transforms an integer into string
+ * @n: input integer
+ *
+ * Return: address of string
+ */
+
+char *to_string(int n)
+{
+	char *s;
+	int i, f = 0;
+	unsigned long t = 10, n2 = n;
+
+	s = malloc(20);
+	if (n == 0)
+	{
+		s[0] = n + '0';
+		s[1] = '\0';
+		return (s);
+	}
+	if (n < 0)
+	{
+		n2 = -n2;
+		f = 1;
+	}
+	for (i = 0; (n2 / (t / 10)) != 0; i++)
+	{
+		s[i] = ((n2 % t) / (t / 10)) + '0';
+		t *= 10;
+	}
+	if (f == 1)
+	{
+		s[i] = '-';
+		s[i + 1] = '\0';
+	}
+	else
+		s[i] = '\0';
+	str_rev(s);
+	return (s);
+}
+
+/**
+ * str_rev - reverses a string
+ * @s: input string
+ *
+ * Return: void
+ */
+
+void str_rev(char *s)
+{
+	int j, k = 0;
+	char m;
+
+	j = str_len(s);
+	for (j = j - 1; k < j; j--, k++)
+	{
+		m = *(s + j);
+		*(s + j) = *(s + k);
+		*(s + k) = m;
+	}
+}
