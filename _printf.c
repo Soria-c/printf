78b740d7bfa0eca5a4b591599f4ca8256ecc99a8
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-14 13:44:02 -0700

    Added description and betty style

--- _printf.c
+++ _printf.c
@@ -1,30 +1,41 @@
 #include "main.h"
 
+/**
+ * _printf - prints a formatted string
+ * @format: imput string
+ *
+ * Return: number of bytes if is SUCCESS, -1 otherwise.
+ */
+
 int _printf(const char *format, ...)
 {
 	va_list args;
- 	char *f_check, *init;
-	int i, lenght, k;
+	char *f_check, init[1024];
+	int i, lenght, bytes, s, k;
 
 	f_check = (char *)format;
 	lenght = str_len(f_check);
 	va_start(args, format);
 	for (i = 0; *f_check != '%' && *f_check != 0; i++, f_check++)
-		;
+		continue;
 	if (i == lenght)
 	{
 		write(1, format, lenght);
-		return (0);
+		return (lenght);
 	}
 	else
 	{
-		init = malloc(lenght + 1);
-		for (k = 0; k < lenght; k++)
-			init[k] = 'a';
-		init[k] = 0; 
-		f_select(format, init, f_check, i, args, 0);
+		for (k = 0; k < 1024; k++)
+			init[k] = 'r';
+		init[k] = '\0';
+		s = f_sel(format, init, f_check, i, args, 0);
 	}
-	write(1, init, str_len(init));
-	free(init);
-	return (0);
+	if (s != 0)
+	{
+		write(2, "Unsupported format\n", 20);
+		return (-1);
+	}
+	bytes = str_len(init);
+	write(1, init, bytes);
+	return (bytes);
 }
