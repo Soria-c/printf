13521e77fc8757083899d4b212bc7248e2f35aa8
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-14 13:50:33 -0700

    Now handles %c

--- print_char.c
+++ print_char.c
@@ -1,21 +1,37 @@
 #include "main.h"
 
-void print_char(const char *format, char *f_s, int sz, va_list args, int num_opt, int z)
+/**
+ * print_c - copies args in f
+ * @format: input string
+ * @f: buffer
+ * @sz: number of bytes before %
+ * @args: arguments list
+ * @op: operation number
+ * @z: offset
+ *
+ * Return: 0 if f_sel is SUCCESS and 1 otherwise.
+ */
+
+int print_c(const char *format, char *f, int sz, va_list args, int op, int z)
 {
-	int copy, i, j;
+	int copy, i, j, r;
 	char *f_check;
 
+	j = r = 0;
 	for (copy = 0; copy < sz; copy++)
-		f_s[copy] = format[z + copy];
-	f_s[copy] = va_arg(args, int);
+		f[copy] = format[z + copy];
+	f[copy] = va_arg(args, int);
 	j = copy + 1;
-	for (; format[copy + z + num_opt + 1] != '\0'; copy++)
-		f_s[copy + 1] = format[copy + z + num_opt + 1];
-	f_s[copy + 1] = '\0';
-	f_check = f_s;
-	f_s = f_s + j;
+	for (; format[copy + z + op + 1] != '\0'; copy++)
+		f[copy + 1] = format[copy + z + op + 1];
+	f[copy + 1] = '\0';
+	f = f + j;
+	f_check = f;
 	for (i = 0; *f_check != '%' && *f_check != 0; i++, f_check++)
-		;
-	z = z + sz + num_opt + 1;
-	f_select(format, f_s, f_check, i - j, args, z);
+		continue;
+	z = z + sz + op + 1;
+	r = f_sel(format, f, f_check, i, args, z);
+	if (r == 1)
+		return (r);
+	return (r);
 }
