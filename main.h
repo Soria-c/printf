057d92660cf27d1820cc8d111647708154de0cda
Carlos Soria <cs227772@gmail.com>
2022-03-15 12:30:05 -0500

    Added one more prototype and removed unused libraries

--- main.h
+++ main.h
@@ -3,7 +3,6 @@
 
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 int _printf(const char *format, ...);
 int str_len(char *str);
@@ -12,7 +11,9 @@ int print_c(const char *format, char *f, int sz, va_list args, int op, int z);
 int print_d(const char *format, char *f, int sz, va_list args, int op, int z);
 int print_p(const char *format, char *f, int sz, va_list args, int op, int z);
 int print_s(const char *format, char *f, int sz, va_list args, int op, int z);
+int print_v(const char *format, char *f, int sz, va_list args, int op, int z);
 void str_rev(char *s);
 void to_string(char *s, int n);
+int check(char *fs, char *s, int *op, int sz);
 
 #endif /*MAIN_H*/
