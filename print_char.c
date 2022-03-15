5b795a415c8c9a17ba7f64606469e6d7898e0009
Carlos Soria <cs227772@gmail.com>
2022-03-15 12:32:19 -0500

    Added a new function print_v and changed return value

--- print_char.c
+++ print_char.c
@@ -9,7 +9,7 @@
  * @op: operation number
  * @z: offset
  *
- * Return: 0 if f_sel is SUCCESS and 1 otherwise.
+ * Return: -1 is special in f_sel is triggered, 0 otherwise.
  */
 
 int print_c(const char *format, char *f, int sz, va_list args, int op, int z)
@@ -33,7 +33,42 @@ int print_c(const char *format, char *f, int sz, va_list args, int op, int z)
 		continue;
 	z = z + sz + op + 1;
 	r = f_sel(format, f, f_check, i, args, z);
-	if (r == 1)
+	if (r == -1)
 		return (r);
 	return (r);
+}
+
+/**
+ * print_v - copies args in f
+ * @format: input string
+ * @f: buffer
+ * @sz: number of bytes before %
+ * @args: arguments list
+ * @op: operation number
+ * @z: offset
+ *
+ * Return: -1 is special in f_sel is triggered, 0 otherwise.
+ */ 
+int print_v(const char *format, char *f, int sz, va_list args, int op, int z)
+{
+	int copy, i, j, r;
+	char *f_check;
+
+	j = r = 0;
+	for (copy = 0; copy < sz; copy++)
+		f[copy] = format[z + copy];
+	f[copy] = format[z + copy];
+	j = copy;
+	if (format[z + copy + 1] == ' ')
+		r = 1;
+	for (; format[copy + z + op - r] != '\0'; copy++)
+		f[copy + 1] = format[copy + z + op - r];
+	f[copy + 1] = '\0';
+	f = f + j + 2 + r;
+	f_check = f;
+	for (i = 0; *f_check != '%' && *f_check != 0; i++, f_check++)
+		continue;
+	z = z + sz + op + 1;
+	f_sel(format, f, f_check, i, args, z);
+	return (0);
 }
