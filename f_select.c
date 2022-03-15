6a4b3ae5cb06873047bf1737848588e819fa55b0
Your Name <yohel.cruz.espinoza@gmail.com>
2022-03-14 20:36:27 -0700

    Restored previous version

--- f_select.c
+++ f_select.c
@@ -15,24 +15,23 @@
 int f_sel(const char *format, char *fs, char *s, int sz, va_list args, int z)
 {
 	int op, i, of = 2;
-	char *ff = (char *)format;
+	char *ff;
 
+	ff = (char *)format;
 	if (*s == '\0')
 		return (0);
 	for (op = 0; s[op] < 'a'; op++)
-	{
-		if (s[op + 1] == '%')
-		{
-			op++;
-			break;
-		}
-	}
+		continue;
+	if (s[1] == '%')
+		op = 1;
 	switch (s[op])
 	{
 		case 'c':
 			print_c(format, fs, sz, args, op, z);
 			break;
 		case 'd':
+			print_d(format, fs, sz, args, op, z);
+			break;
 		case 'i':
 			print_d(format, fs, sz, args, op, z);
 			break;
@@ -47,8 +46,9 @@ int f_sel(const char *format, char *fs, char *s, int sz, va_list args, int z)
 				of = 1;
 			if (str_len(ff) == 2)
 				return (2);
-			s = fs = fs + op + of;
+			fs = fs + op + of;
 			z = z + op + of;
+			s = fs;
 			for (i = 0; *s != '%' && *s != '\0'; i++, s++)
 				continue;
 			f_sel(format, fs, s, i, args, z);
