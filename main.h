65df4e1115dda9ec46c8597cfe2f76f3e1bc5b4c
yohelce <yohel.cruz.espinoza@gmail.com>
2022-03-15 22:12:45 -0500

    Fixed function's names

--- main.h
+++ main.h
@@ -7,10 +7,10 @@
 int _printf(const char *format, ...);
 int str_len(char *str);
 int f_sel(const char *format, char *fs, char *s, int sz, va_list args, int z, int w);
-int print_c(const char *format, char *f, int sz, va_list args, int op, int z, int w);
-int print_d(const char *format, char *f, int sz, va_list args, int op, int z, int w);
-int print_p(const char *format, char *f, int sz, va_list args, int op, int z, int w);
-int print_s(const char *format, char *f, int sz, va_list args, int op, int z, int w);
+int p_c(const char *format, char *f, int sz, va_list args, int op, int z, int w);
+int p_d(const char *format, char *f, int sz, va_list args, int op, int z, int w);
+int p_p(const char *format, char *f, int sz, va_list args, int op, int z, int w);
+int p_s(const char *format, char *f, int sz, va_list args, int op, int z, int w);
 int print_v(const char *format, char *f, int sz, va_list args, int op, int z, int w);
 void str_rev(char *s);
 void to_string(char *s, int n);
